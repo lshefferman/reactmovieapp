882006fc2f0132d00170b804a5e2144ce7cc062b
Leah Shefferman <ls0757@princeton.edu>
2025-04-03 01:31:27 -0400

    almost done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,46 @@
 import MovieCard from "../components/MovieCard"
-import {useState} from "react"
+import {useState, useEffect} from "react"
+import { searchMovies, getPopularMovies} from "../services/api"
 import "../css/Home.css"
 
 function Home() {
     const [searchQuery, setSearchQuery] = useState("");
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true)
 
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies()
+                setMovies(popularMovies)
+            } catch (err) {
+                console.log(err)
+                setError("Failed to load movies...")
+            }
+            finally {
+                setLoading(false)
+            }
+        }
+    }, [])
 
-    const movies = [
-        {id: 1, title: "John Wick", release_date: "2020"},
-        {id: 2, title: "Terminator", release_date: "1999"}
-    ];
+    const handleSearch = async (e) => {
+        e.preventDefault()
+        if (!searchQuery.trim()) return
+        if (loading) return
 
-const handleSearch = () => {
-    e.preventDefault()
-    alert(searchQuery)
-    setSearchQuery("")
-}
+        setLoading(true)
+        try {
+            const searchResults = await searchMovies(searchQuery)
+            setMovies(searchResults)
+            setError(null)
+        } catch (err) {
+            console.log(err)
+            setError("Failed to search movies...")
+        } finally {
+            setLoading(false)
+        }
+    }
 
 
     return (
@@ -34,12 +59,15 @@ const handleSearch = () => {
         </form>
 
 
+        {loading ? (
+            <div className="loading">loading...</div>
+        ) : (
         <div className="movies-grid">
             {movies.map(
                 (movie) => 
                 (<MovieCard movie ={movie} key={movie.id} />
             ))}
-        </div>
+        </div> )}
     </div>
     );
 }
